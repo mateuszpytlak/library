1e9cdb4f2c7dd4e358e469e794d49023ac3c1d1e
Mateusz Pytlak <mateusz.pytlak@af.agency>
2019-04-08 11:03:19 +0200

    vue composed

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+	<div id="app">
+		<input v-model="title" type="text">
+		<button @click.prevent="search">Search</button>
+	</div>
+</template>
+
+<script>
+
+	export default {
+		name: 'app',
+		data () {
+			return {
+				title: '',
+			}
+		},
+		methods: function search() {
+			console.log('search');
+			console.log(this.title);
+		}
+}
+
+</script>
+
+<style lang="scss">
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+h1, h2 {
+	font-weight: normal;
+}
+
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+
+a {
+	color: #42b983;
+}
+
+</style>
