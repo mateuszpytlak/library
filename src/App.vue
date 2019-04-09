0171db66c66e47335600c5203c840e78b89ed889
Mateusz Pytlak <mateusz.pytlak@af.agency>
2019-04-09 12:29:09 +0200

    loading more book @ scroll bottom

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-	<div id="app">
-		<input @keydown.enter="fetchData" v-model="inputTitle" type="text">
-		<button @click="fetchData">Search</button>
+	<div class="container" id="app">
+		<input @keydown.enter="fetchData(10); resetCount();" v-model="inputTitle" type="text" placeholder="What are you looking for?">
+		<button @click="fetchData(10); resetCount()">Search</button>
 		<app-book-list 
 			v-for="(title, index) in titlesArray" 
 			:title="title" 
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+
 
 	import BookList from './components/BookList.vue';
 
@@ -20,6 +21,8 @@
 				inputTitle: '',
 				data: '',
 				titlesArray: [],
+				bottom: false,
+				countResults: 10,
 			}
 		},
 		components: {
@@ -30,26 +33,75 @@
 				// console.log('search: ' + this.inputTitle);
 				this.inputTitle = '';
 			},
-			fetchData() {
-				const url = `https://www.googleapis.com/books/v1/volumes?q=${this.inputTitle}`;
+			fetchData(num) {
+				const url = `https://www.googleapis.com/books/v1/volumes?q=${this.inputTitle}&maxResults=${num}`;
 				console.log(url);
-				this.$http.get(`https://www.googleapis.com/books/v1/volumes?q=${this.inputTitle}`)
+				this.$http.get(url)
 					.then(response => {
 						return response.json();
 					})
 					.then(data => {
 						console.log(data.items);
 						this.titlesArray = data.items;
-						this.inputTitle = '';
+						// this.inputTitle = '';
 					}
 					);
+			},
+			bottomVisible() {
+				const scrollY = window.scrollY;
+				// console.log('window.scrollY: ' + scrollY);
+				const visible = document.documentElement.clientHeight;
+				// console.log('document.documentElement.clientHeight: ' + visible);
+				const pageHeight = document.documentElement.scrollHeight;
+				// console.log('document.documentElement.scrollHeight: ' + pageHeight);
+				const bottomOfPage = visible + scrollY >= pageHeight;
+				// console.log(bottomOfPage);
+				// console.log('is page at the bottom? ' + bottomOfPage || pageHeight < visible);
+				return bottomOfPage || pageHeight < visible;
+			},
+			resetCount() {
+				this.countResults = 10;
 			}
+		},
+		watch: {
+			bottom(bottom) {
+				if(bottom) {
+					if(this.countResults < 40) {
+						this.countResults += 10;
+					}
+					this.fetchData(this.countResults);
+				}
+			}
+		},
+		created() {
+			window.addEventListener('scroll', () => {
+				this.bottom = this.bottomVisible();
+			})
 		}
 }
 
 </script>
 
 <style lang="scss">
+
+* {
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+}
+
+.container {
+	width: 70%;
+	border: 2px dashed green;
+	padding-left: 30px;
+	padding-right: 30px;
+	margin-left: auto;
+	margin-right: auto;
+	input {
+		margin: 20px 0;
+	}
+}
+
 #app {
 	font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,31 +5,39 @@
 		</div>
 		<div class="description" @click="showMore">
 			<h2>{{ title.volumeInfo.title }}</h2>
-			<p>{{ shortDesctipt }}</p>
-			<button v-if="isShowMoreVisible" @click="showMore">Show more</button>
+			<p>{{ shortDesctipt }}</p><p class="showMore" v-if="isShowMoreVisible" @click="showMore">Show more</p>
 		</div>
 	</div>
 </template>
 
 <script>
 
+// const description = this.title.volumeInfo.description;
+// const imgLinks = this.title.volumeInfo.imageLinks;
+
+
+
 	export default {
 		props: ['title'],
 		data() {
 			return {
-				src: this.title.volumeInfo.imageLinks.thumbnail,
-				shortDesctipt: '',
+				src: typeof this.title.volumeInfo.imageLinks === 'undefined' ? '' : this.title.volumeInfo.imageLinks.thumbnail,
+				shortDesctipt: typeof this.title.volumeInfo.description === 'undefined' ? '' : this.title.volumeInfo.description,
 				isShowMoreVisible: false,
 			}
 		},
 		created: function() {
 			const description = this.title.volumeInfo.description;
+			// console.log('created');
 			if(typeof description !== 'undefined') {
+				// console.log('defined');
 				if(description.length > 199) {
 					this.shortDesctipt = description.slice(0, 200);
 					this.isShowMoreVisible = true;
+					console.log('show button show');
 				}
 			} else {
+				// console.log('undefined');
 				this.shortDesctipt = '';
 			}
 		},
@@ -44,11 +52,28 @@
 				// console.log('niegit');
 				this.src ='';
 			}
+			
+			const description = this.title.volumeInfo.description;
+			// console.log('created');
+			if(typeof description !== 'undefined') {
+				// console.log('defined');
+				if(description.length > 199 && this.isShowMoreVisible) {
+					this.shortDesctipt = description.slice(0, 200);
+					this.isShowMoreVisible = true;
+				}
+			} else {
+				// console.log('undefined');
+				this.shortDesctipt = '';
+			}
+
 		},
 		methods: {
 			showMore() {
+				// console.log('show me more!');
+				// console.log(this.shortDesctipt);
 				const description = this.title.volumeInfo.description;
 				this.shortDesctipt = description;
+				// console.log(this.shortDesctipt);
 				this.isShowMoreVisible = false;
 			}
 		}
@@ -60,9 +85,11 @@
 <style lang="scss">
 
 .bookList {
-	border: 1px solid red;
+	// border: 1px solid red;
 	display: flex;
 	min-height: 200px;
+	margin-bottom: 20px;
+	// border-bottom: 2px solid lightgray;
 }
 
 .image {
@@ -77,6 +104,13 @@
 .description {
 	margin-left: 100px;
 	text-align: left;
+	border: 1px solid blue;
+}
+
+.showMore {
+	color: gray;
+	text-decoration: underline;
+	cursor: pointer;
 }
 
 </style>
